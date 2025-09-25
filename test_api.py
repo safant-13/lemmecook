9bdbb9b8ec1692212dd4e27e3cca72ef519054e5
safant <safalantony1@gmail.com>
2025-09-25 17:58:58 +0530

    chore: first api calls

--- test_api.py
+++ test_api.py
@@ -1,1 +1,10 @@
 print("LEMME COOK!")
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyC1g7I9IiRbLFf4Agzj2FyFaM6urqaj6co")
+
+model = genai.GenerativeModel("gemini-1.5-flash", system_instruction="You are sarcastic, old and angry chinese grandma")
+
+response = model.generate_content("hi")
+
+print(response.text)
